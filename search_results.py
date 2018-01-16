3ea2d489f346f7a1d3914ea28e6d0be6c9a965f8
decordoba <danieldecordobagil@hotmail.com>
2018-01-16 15:52:07 +0100

    Search results now can also sort by result, not only by param

--- search_results.py
+++ search_results.py
@@ -5,39 +5,8 @@ import os
 import yaml
 
 
-def search_results(folder=None, pause_in_every_result=True):
-    """
-    From the selected folder (or current folder if folder == None), filter results according to
-    result.yaml
-    """
-
-    # Navigate to folder and load result.yaml
-    if folder is not None:
-        os.chdir(folder)
-    with open("results.yaml") as f:
-        try:
-            result = yaml.load(f)
-        except yaml.YAMLError as YamlError:
-            print("There was an error parsing 'results.yaml'. Plotting aborted.")
-            print(YamlError)
-            if folder is not None:
-                os.chdir("./..")
-            return
-
-    # Extract params from result, which will save all possible values for every key in params
-    parameters = {}
-    for sample_key in result:
-        sample = result[sample_key]
-        for key in sample["params"]:
-            if key not in parameters:
-                parameters[key] = set()
-            parameters[key].add(sample["params"][key])
-
-    # params_keys will hold all the plotable keys
-    params_keys = sorted(parameters.keys())
-    params_dict = dict(zip(params_keys, range(len(params_keys))))
-    params_values = []
-
+def filter_by_params(parameters, result, params_keys, params_values, params_dict,
+                     pause_in_every_result=True):
     # Ask the users what variables to filter
     print("Select the search values for every parameter.")
     print("Leave blank (press ENTER) to ignore a parameter.")
@@ -73,8 +42,122 @@ def search_results(folder=None, pause_in_every_result=True):
             if pause_in_every_result:
                 input("Press ENTER to see next result")
             print(" ")
+
+
+def filter_by_result(parameters, result, result_keys, pause_in_every_result=True):
+    # Ask the users what variables to filter
+    print("Select the result variable that will be used to filter.")
+    print("0. Exit")
+    for i, key in enumerate(result_keys):
+        print("{}. {}", format(i + 1, key))
+    num = -1
+    while num < 0 or num > len(result_keys):
+        try:
+            num = int(input(">> "))
+        except ValueError:
+            num = -1
+    if num == 0:
+        return
+    param = result_keys[num - 1]
+    print("Select what experiments you want to find.")
+    print("0. Exit\n1. With MAX {}\n2. With MIN {}".format(param, param))
+    num = -1
+    while num < 0 or num > 2:
+        try:
+            num = int(input(">> "))
+        except ValueError:
+            num = -1
+    if num == 0:
+        return
+    sign = (num == 1)
+    print("Select how many experiments you want to see.")
+    num = -1
+    while num < 0 or num > 2:
+        try:
+            num = int(input(">> "))
+        except ValueError:
+            num = -1
+    if num == 0:
+        return
+
+    # Get sorted results
+    sample_keys = []
+    sample_values = []
+    for sample_key in result:
+        sample = result[sample_key]
+        sample_keys.append(sample_key)
+        sample_values.append(sample["result"][param])
+    sorted_keys = [x for _, x in sorted(zip(sample_values, sample_keys), reversed=sign)]
+
+    # Print results
+    for sample_key in sorted_keys[:num]:
+        sample = result[sample_key]
+        print("Sample {}:".format(sample_key))
+        print("---------------------------------------------------")
+        for key in sorted(sample["params"]):
+            print("  {:>20}: {}".format(key, sample["params"][key]))
+        print("---------------------------------------------------")
+        for key in sorted(sample["result"]):
+            print("  {:>20}: {}".format(key, sample["result"][key]))
+        print("---------------------------------------------------")
+        if pause_in_every_result:
+            input("Press ENTER to see next result")
+        print(" ")
+
+
+def search_results(folder=None, pause_in_every_result=True):
+    """
+    From the selected folder (or current folder if folder == None), filter results according to
+    result.yaml
+    """
+
+    # Navigate to folder and load result.yaml
+    if folder is not None:
+        os.chdir(folder)
+    with open("results.yaml") as f:
+        try:
+            result = yaml.load(f)
+        except yaml.YAMLError as YamlError:
+            print("There was an error parsing 'results.yaml'. Plotting aborted.")
+            print(YamlError)
+            if folder is not None:
+                os.chdir("./..")
+            return
     if folder is not None:
         os.chdir("./..")
+
+    # Extract params from result, which will save all possible values for every key in params
+    parameters = {}
+    for sample_key in result:
+        sample = result[sample_key]
+        for key in sample["params"]:
+            if key not in parameters:
+                parameters[key] = set()
+            parameters[key].add(sample["params"][key])
+
+    # save all params and results required
+    params_keys = sorted(parameters.keys())
+    params_dict = dict(zip(params_keys, range(len(params_keys))))
+    params_values = []
+    result_keys = sorted(result[sample_key]["result"].keys())
+
+    while True:
+        print("Select what to do:")
+        print("0. Exit\n1. Filter by params\n2. Filter by result")
+        num = -1
+        while num < 0 or num > 2:
+            try:
+                num = int(input(">> "))
+            except ValueError:
+                num = -1
+        if num == 0:
+            break
+        elif num == 1:
+            filter_by_params(parameters, result, params_keys, params_values, params_dict,
+                             pause_in_every_result=pause_in_every_result)
+        else:
+            filter_by_result(parameters, result, params_keys, params_values, params_dict,
+                             pause_in_every_result=pause_in_every_result)
 
 
 if __name__ == "__main__":
