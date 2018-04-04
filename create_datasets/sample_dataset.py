74403968adba75d3ba1dd083862803a52f8fa3a4
decordoba <danieldecordobagil@hotmail.com>
2018-04-04 17:01:50 -0500

    Add functions to scale and translate volumes

--- create_datasets/sample_dataset.py
+++ create_datasets/sample_dataset.py
@@ -2,9 +2,10 @@
 import argparse
 import numpy as np
 from skimage import transform
-# import sys
-# sys.path.insert(0, '..')
-# from calculate_dataset_statistics import read_dataset
+from scipy import ndimage
+import sys
+sys.path.insert(0, '..')
+from calculate_dataset_statistics import read_dataset
 
 
 def get_3_medians(volume, mask):
@@ -36,12 +37,13 @@ def convert_volumes_to_medians(volumes, masks):
     return new_volumes, new_masks
 
 
-def rotate_randomly(volume, mask):
+def rotate_randomly(volume, mask, rotation=None):
     """Rotate volume and mask randomly."""
-    rotations = np.random.random(3) * 360
+    if rotation is None:
+        rotation = np.random.random(3) * 360
     rotated_volume = volume
     rotated_mask = mask
-    for i, theta in enumerate(rotations):
+    for i, theta in enumerate(rotation):
         rotated_volume = transform.rotate(rotated_volume, theta)
         rotated_mask = transform.rotate(rotated_mask, theta)
         if i == 0:
@@ -50,30 +52,92 @@ def rotate_randomly(volume, mask):
         elif i == 1:
             rotated_volume = np.rot90(rotated_volume, axes=(0, 2))
             rotated_mask = np.rot90(rotated_mask, axes=(0, 2))
-    return rotated_volume, rotated_mask
+    return rotated_volume, rotated_mask, rotation
 
 
-def main(dataset, samples_per_image, size=None, plot_slices=False, plot=False):
-    """Sample dataset using 3 medians."""
-    pass
-    # Load dataset
-    # volumes, labels, masks, patients = read_dataset(dataset, size, plot_slices, plot)
-    #
-    # samples_x = []
-    # samples_y = []
-    # samples_m = []
-    # samples_p = []
-    # for i, (x, y, m, p) in enumerate(zip(volumes, labels, masks, patients)):
-    #     for j in range(samples_per_image):
-    #         if j == 0:
-    #             x_pt, m_pt = get_3_medians(x, m)
-    #         else:
-    #             x_pt, m_pt = rotate_randomly(x, m)
-    #             x_pt, m_pt = get_3_medians(x_pt, m_pt)
-    #         samples_x.append(x_pt)
-    #         samples_m.append(m_pt)
-    #         samples_y.append(y)
-    #         samples_p.append(p)
+def translate_randomly(volume, mask, translation=None, max_distance=5):
+    """Translate (move) volume and mask randomly."""
+    if translation is None:
+        dist = np.square(np.random.random() * max_distance)
+        translation = np.zeros(3)
+        translation[0] = np.random.random() * dist
+        translation[1] = np.random.random() * (dist - translation[0])
+        translation[2] = dist - translation[0] - translation[1]
+        translation = np.sqrt(translation) * ((np.random.randint(0, 2, 3) * 2) - 1)
+    translated_volume = volume
+    translated_mask = mask
+    for i, dist in enumerate(translation):
+        minv = int(dist)
+        maxv = minv + 1
+        maxf = dist - minv
+        minf = 1 - maxf
+        shift_min = np.zeros(3)
+        shift_min[i] = minv
+        shift_max = np.zeros(3)
+        shift_max[i] = maxv
+        translated_volume = (ndimage.interpolation.shift(translated_volume, shift_min) * minf +
+                             ndimage.interpolation.shift(translated_volume, shift_max) * maxf)
+        translated_mask = (ndimage.interpolation.shift(translated_mask, shift_min) * minf +
+                           ndimage.interpolation.shift(translated_mask, shift_max) * maxf)
+    return translated_volume, translated_mask, translation
+
+
+def scale_volume(volume, mask, scales=(1, 1.2, 1.4, 1.6)):
+    """Scale volume and mask, and cut it to have same shape as original.
+
+    Scales must be greater or equal to 1. Volume and mask must have same shape.
+    """
+    scaled_volumes = []
+    scaled_masks = []
+    for scale in scales:
+        scaled_volume = ndimage.zoom(volume, scale)
+        scaled_mask = ndimage.zoom(mask, scale)
+        ml = ((np.array(scaled_volume.shape) - volume.shape) / 2).astype(int)
+        mr = ml + volume.shape
+        scaled_volumes.append(scaled_volume[ml[0]:mr[0], ml[1]:mr[1], ml[2]:mr[2]])
+        scaled_masks.append(scaled_mask[ml[0]:mr[0], ml[1]:mr[1], ml[2]:mr[2]])
+    return scaled_volumes, scaled_masks
+
+
+def augment_dataset(volumes, labels, masks, patients, scale_samples=(1, 1.2, 1.4, 1.6),
+                    num_rotate_samples=5, num_translate_samples=5):
+    """Augment dataset scaling, translating and rotating."""
+    samples_x = []
+    samples_y = []
+    samples_m = []
+    samples_p = []
+    rotations = [(0, 0, 0)]
+    translations = [(0, 0, 0)]
+    max_distance = 4
+    for i, (x, y, m, p) in enumerate(zip(volumes, labels, masks, patients)):
+        # Create scaled volumes
+        augmented_x1, augmented_m1 = scale_volume(x, m, scales=scale_samples)
+        # Create rotations of scaled volumes
+        augmented_x2, augmented_m2 = [], []
+        for j in range(num_rotate_samples):
+            for aug_x, aug_m in zip(augmented_x1, augmented_m1):
+                try:
+                    tmp_x, tmp_m, _ = rotate_randomly(aug_x, aug_m, rotation=rotations[j])
+                except IndexError:
+                    tmp_x, tmp_m, rot = rotate_randomly(aug_x, aug_m)
+                    rotations.append(rot)
+                augmented_x2.append(tmp_x)
+                augmented_m2.append(tmp_m)
+        augmented_x3, augmented_m3 = [], []
+        for j in range(num_translate_samples):
+            for aug_x, aug_m in zip(augmented_x2, augmented_m2):
+                try:
+                    tmp_x, tmp_m, _ = translate_randomly(aug_x, aug_m, translation=translations[j])
+                except IndexError:
+                    tmp_x, tmp_m, tra = translate_randomly(aug_x, aug_m, max_distance=max_distance)
+                    translations.append(tra)
+                augmented_x3.append(tmp_x)
+                augmented_m3.append(tmp_m)
+        samples_x += augmented_x3
+        samples_m += augmented_m3
+        samples_y += [y] * len(augmented_x3)
+        samples_p += [p] * len(augmented_x3)
+    return samples_x, samples_y, samples_m, samples_p, rotations, translations
 
 
 def parse_arguments():
@@ -94,4 +158,7 @@ def parse_arguments():
 
 if __name__ == "__main__":
     args = parse_arguments()
-    main(args.dataset, args.num_samples, args.size, args.plot_slices, args.plot)
+    # Load dataset
+    volumes, labels, masks, patients = read_dataset(args.dataset, args.size, args.plot_slices,
+                                                    args.plot)
+    augment_dataset(volumes, labels, masks, patients, args.num_samples)
