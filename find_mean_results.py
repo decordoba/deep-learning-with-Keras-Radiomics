8c208a77e1537768ad7550d95c11e8d97f155c4a
decordoba <danieldecordobagil@hotmail.com>
2018-04-08 18:03:50 -0500

    Improve plots

--- find_mean_results.py
+++ find_mean_results.py
@@ -43,6 +43,10 @@ def main(correction):
     last_idx = len(all_data1) - 1
     wider_figsize = list(plt.rcParams.get('figure.figsize'))
     wider_figsize[0] += 2.1
+    x_axis = [16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
+    x_scale = "log"
+    x_label = "Number of Patients in Training Set"
+    x_history = range(1, 50 + 1)
     for i, (params1, params2, params3) in enumerate(zip(all_data1, all_data2, all_data3)):
         comb1, all_cv1, all_train1, all_pat1, history1, rocs1 = params1
         comb2, all_cv2, all_train2, all_pat2, history2, rocs2 = params2
@@ -51,66 +55,66 @@ def main(correction):
 
         figsize = wider_figsize if i == 0 else None
 
-        plot_line((np.array(all_cv1["accuracy"]) + all_cv2["accuracy"] + all_cv3["accuracy"]) / 3,
-                  label=str(comb), fig_num=0, show=show_plots, marker=".",
+        plot_line((np.array(all_cv1["accuracy"]) + all_cv2["accuracy"] + all_cv3["accuracy"]) / 3, x_axis,
+                  label=str(comb), fig_num=0, show=show_plots, marker=".", x_scale=x_scale, x_label=x_label,
                   title="{} Accuracy".format(title), legend_out=(i == last_idx), figsize=figsize)
-        plot_line((np.array(all_cv1["recall0"]) + all_cv2["recall0"] + all_cv3["recall0"]) / 3,
-                  label=str(comb), fig_num=1, show=show_plots, marker=".",
+        plot_line((np.array(all_cv1["recall0"]) + all_cv2["recall0"] + all_cv3["recall0"]) / 3, x_axis,
+                  label=str(comb), fig_num=1, show=show_plots, marker=".", x_scale=x_scale, x_label=x_label,
                   title="{} Recall (0)".format(title), legend_out=(i == last_idx), figsize=figsize)
-        plot_line((np.array(all_cv1["recall1"]) + all_cv2["recall1"] + all_cv3["recall1"]) / 3,
-                  label=str(comb), fig_num=2, show=show_plots, marker=".",
+        plot_line((np.array(all_cv1["recall1"]) + all_cv2["recall1"] + all_cv3["recall1"]) / 3, x_axis,
+                  label=str(comb), fig_num=2, show=show_plots, marker=".", x_scale=x_scale, x_label=x_label,
                   title="{} Recall (1)".format(title), legend_out=(i == last_idx), figsize=figsize)
-        plot_line((np.array(all_cv1["precision0"]) + all_cv2["precision0"] + all_cv3["precision0"]) / 3,
-                  label=str(comb), fig_num=3, show=show_plots, marker=".",
+        plot_line((np.array(all_cv1["precision0"]) + all_cv2["precision0"] + all_cv3["precision0"]) / 3, x_axis,
+                  label=str(comb), fig_num=3, show=show_plots, marker=".", x_scale=x_scale, x_label=x_label,
                   title="{} Precision (0)".format(title), legend_out=(i == last_idx),
                   figsize=figsize)
-        plot_line((np.array(all_cv1["precision1"]) + all_cv2["precision1"] + all_cv3["precision1"]) / 3,
-                  label=str(comb), fig_num=4, show=show_plots, marker=".",
+        plot_line((np.array(all_cv1["precision1"]) + all_cv2["precision1"] + all_cv3["precision1"]) / 3, x_axis,
+                  label=str(comb), fig_num=4, show=show_plots, marker=".", x_scale=x_scale, x_label=x_label,
                   title="{} Precision (1)".format(title), legend_out=(i == last_idx),
                   figsize=figsize)
 
-        plot_line((np.array(all_train1["accuracy"]) + all_train2["accuracy"] + all_train3["accuracy"]) / 3,
-                  label=str(comb), fig_num=5, show=show_plots, marker=".",
+        plot_line((np.array(all_train1["accuracy"]) + all_train2["accuracy"] + all_train3["accuracy"]) / 3, x_axis,
+                  label=str(comb), fig_num=5, show=show_plots, marker=".", x_scale=x_scale, x_label=x_label,
                   title="Training Accuracy", legend_out=(i == last_idx), figsize=figsize)
-        plot_line((np.array(all_train1["recall0"]) + all_train2["recall0"] + all_train3["recall0"]) / 3,
-                  label=str(comb), fig_num=6, show=show_plots, marker=".",
+        plot_line((np.array(all_train1["recall0"]) + all_train2["recall0"] + all_train3["recall0"]) / 3, x_axis,
+                  label=str(comb), fig_num=6, show=show_plots, marker=".", x_scale=x_scale, x_label=x_label,
                   title="Training Recall (0)", legend_out=(i == last_idx), figsize=figsize)
-        plot_line((np.array(all_train1["recall1"]) + all_train2["recall1"] + all_train3["recall1"]) / 3,
-                  label=str(comb), fig_num=7, show=show_plots, marker=".",
+        plot_line((np.array(all_train1["recall1"]) + all_train2["recall1"] + all_train3["recall1"]) / 3, x_axis,
+                  label=str(comb), fig_num=7, show=show_plots, marker=".", x_scale=x_scale, x_label=x_label,
                   title="Training Recall (1)", legend_out=(i == last_idx), figsize=figsize)
-        plot_line((np.array(all_train1["precision0"]) + all_train2["precision0"] + all_train3["precision0"]) / 3,
-                  label=str(comb), fig_num=8, show=show_plots, marker=".",
+        plot_line((np.array(all_train1["precision0"]) + all_train2["precision0"] + all_train3["precision0"]) / 3, x_axis,
+                  label=str(comb), fig_num=8, show=show_plots, marker=".", x_scale=x_scale, x_label=x_label,
                   title="Training Precision (0)", legend_out=(i == last_idx), figsize=figsize)
-        plot_line((np.array(all_train1["precision1"]) + all_train2["precision1"] + all_train3["precision1"]) / 3,
-                  label=str(comb), fig_num=9, show=show_plots, marker=".",
+        plot_line((np.array(all_train1["precision1"]) + all_train2["precision1"] + all_train3["precision1"]) / 3, x_axis,
+                  label=str(comb), fig_num=9, show=show_plots, marker=".", x_scale=x_scale, x_label=x_label,
                   title="Training Precision (1)", legend_out=(i == last_idx), figsize=figsize)
 
-        plot_line((np.array(all_pat1["accuracy"]) + all_pat2["accuracy"] + all_pat3["accuracy"]) / 3,
-                  label=str(comb), fig_num=10, show=show_plots, marker=".",
+        plot_line((np.array(all_pat1["accuracy"]) + all_pat2["accuracy"] + all_pat3["accuracy"]) / 3, x_axis,
+                  label=str(comb), fig_num=10, show=show_plots, marker=".", x_scale=x_scale, x_label=x_label,
                   title="{} Patient Accuracy".format(title), legend_out=(i == last_idx),
                   figsize=figsize)
-        plot_line((np.array(all_pat1["recall0"]) + all_pat2["recall0"] + all_pat3["recall0"]) / 3,
-                  label=str(comb), fig_num=11, show=show_plots, marker=".",
+        plot_line((np.array(all_pat1["recall0"]) + all_pat2["recall0"] + all_pat3["recall0"]) / 3, x_axis,
+                  label=str(comb), fig_num=11, show=show_plots, marker=".", x_scale=x_scale, x_label=x_label,
                   title="{} Patient Recall (0)".format(title), legend_out=(i == last_idx),
                   figsize=figsize)
-        plot_line((np.array(all_pat1["recall1"]) + all_pat2["recall1"] + all_pat3["recall1"]) / 3,
-                  label=str(comb), fig_num=12, show=show_plots, marker=".",
+        plot_line((np.array(all_pat1["recall1"]) + all_pat2["recall1"] + all_pat3["recall1"]) / 3, x_axis,
+                  label=str(comb), fig_num=12, show=show_plots, marker=".", x_scale=x_scale, x_label=x_label,
                   title="{} Patient Recall (1)".format(title), legend_out=(i == last_idx),
                   figsize=figsize)
-        plot_line((np.array(all_pat1["precision0"]) + all_pat2["precision0"] + all_pat3["precision0"]) / 3,
-                  label=str(comb), fig_num=13, show=show_plots, marker=".",
+        plot_line((np.array(all_pat1["precision0"]) + all_pat2["precision0"] + all_pat3["precision0"]) / 3, x_axis,
+                  label=str(comb), fig_num=13, show=show_plots, marker=".", x_scale=x_scale, x_label=x_label,
                   title="{} Patient Precision (0)".format(title), legend_out=(i == last_idx),
                   figsize=figsize)
-        plot_line((np.array(all_pat1["precision1"]) + all_pat2["precision1"] + all_pat3["precision1"]) / 3,
-                  label=str(comb), fig_num=14, show=show_plots, marker=".",
+        plot_line((np.array(all_pat1["precision1"]) + all_pat2["precision1"] + all_pat3["precision1"]) / 3, x_axis,
+                  label=str(comb), fig_num=14, show=show_plots, marker=".", x_scale=x_scale, x_label=x_label,
                   title="{} Patient Precision (1)".format(title), legend_out=(i == last_idx),
                   figsize=figsize)
 
-        plot_line((np.array(history1[0]) + history2[0] + history3[0]) / 3,
-                  label=str(comb), fig_num=15, show=show_plots, figsize=figsize,
+        plot_line((np.array(history1[0]) + history2[0] + history3[0]) / 3, x_history,
+                  label=str(comb), fig_num=15, show=show_plots, figsize=figsize, x_label="Epoch",
                   title="Mean Training Accuracy History ", legend_out=(i == last_idx))
-        plot_line((np.array(history1[1]) + history2[1] + history3[1]) / 3,
-                  label=str(comb), fig_num=16, show=show_plots, figsize=figsize,
+        plot_line((np.array(history1[1]) + history2[1] + history3[1]) / 3, x_history,
+                  label=str(comb), fig_num=16, show=show_plots, figsize=figsize, x_label="Epoch",
                   title="Mean Test Accuracy History", legend_out=(i == last_idx))
     # Save PDF results
     save_plt_figures_to_pdf("figures_median.pdf")
